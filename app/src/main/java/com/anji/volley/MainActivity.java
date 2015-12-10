d267c8fb66b13e58e3ef024f89aa3d68c7564b79
sagarika <sagarika.nagunoori@gmail.com>
2015-12-10 17:46:56 +0530

    code changed

--- app/src/main/java/com/anji/volley/MainActivity.java
+++ app/src/main/java/com/anji/volley/MainActivity.java
@@ -1,52 +1,66 @@
 package com.anji.volley;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class MainActivity extends Activity {
+    TextView textView;
+    Button btnRequest;
+    RequestQueue queue;
+    StringRequest stringRequest;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+         textView=(TextView)findViewById(R.id.text_hello);
+        btnRequest = (Button)findViewById(R.id.btn_send_request);
+
+
+        // Instantiate the RequestQueue
+//        queue= Volley.newRequestQueue(this);
+        queue=Singleton.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        String url="http://involgix.com/poultryone/mobileapp/login.php?username=9948562267&password=727298";
+        // Request a string response from the provided URL.
+        stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onResponse(String response) {
+                Log.d("Volley",response);
+                textView.setText(response);
+            }
+        },new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText(error.getMessage());
             }
         });
+
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+//                Singleton.getInstance(this).addToRequestQueue(stringRequest);
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
